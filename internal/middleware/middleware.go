298d6d1fd5d9e159f2fd6d208e43318fd07b6aa5
Maxim Vasilev <msprog@icloud.com>
2026-09-01 19:53:39 +0400

    feat: add request id logging middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,7 +1,10 @@
 package middleware
 
 import (
+	"delivery-api/internal/logger"
+	chimw "github.com/go-chi/chi/v5/middleware"
 	"log"
+	"log/slog"
 	"net/http"
 	"time"
 )
@@ -32,4 +35,16 @@ func Recover(next http.Handler) http.Handler {
 		}()
 		next.ServeHTTP(w, r)
 	})
+}
+
+func RequestID(baseLogger *slog.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			id := chimw.GetReqID(ctx)
+			ctx = logger.WithLogger(ctx, baseLogger.With(slog.String("request_id", id)))
+			req := r.WithContext(ctx)
+			next.ServeHTTP(w, req)
+		})
+	}
 }
